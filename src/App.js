53597cc2aa0cd1369dde7b55a084f7a106a35bb9
Gerard Eklu <gerard_eklu@homedepot.com>
2019-07-11 02:05:14 -0400

    Added style to the profile user

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
 import React, { Component } from "react";
-// import Profile from "./components/profile/profile";
+import Profile from "./components/profile/profile";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      names: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://randomuser.me/api/?results=10")
-      .then(results => {
-        return results.json();
-      })
-      .then(data => {
-        let names = data.results.map(name => {
-          return (
-            <div key={name.results}>
-              <h4>
-                {name.name.first} {name.name.last}
-              </h4>
-              <br />
-              <img src={name.picture.large} alt="pic" />
-            </div>
-          );
-        });
-
-        this.setState({ names: names });
-        console.log(this.state.names);
-      });
-  }
-
   render() {
     return (
       <div className="App">
-        <h2>Profile Site</h2>
-        {this.state.names}
+        <Profile />
       </div>
     );
   }
--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import "./profile.css";
 
-const Profile = () => {
-  return (
-    <div>
-      <h3>Fetch Profile</h3>
-    </div>
-  );
-};
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      names: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=40")
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        let names = data.results.map(name => {
+          return (
+            <div key={name.results} className="profile">
+              <h4 className="name">
+                {name.name.first} {name.name.last}
+              </h4>
+              <br />
+              <img src={name.picture.large} alt="pic" />
+            </div>
+          );
+        });
+
+        this.setState({ names: names });
+        console.log(this.state.names);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="user">Profile Users</h2>
+        <h4 className="list">A list of random profile users</h4>
+        {this.state.names}
+      </div>
+    );
+  }
+}
 
 export default Profile;
